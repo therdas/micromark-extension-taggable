634a71c240476d2dd4b627950759365d02776895
therdas <git@therdas.dev>
2024-12-02 20:04:19 +0530

    Fixed import errors

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,15 @@
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import PeerDepsExternal from "rollup-plugin-peer-deps-external";
-import copy from "rollup-plugin-copy"
+import copy from "rollup-plugin-copy";
 
 const config = {
   input: "src/index.js",
   output: [
+    {
+      file: "dist/index.js",
+      format: "esm",
+    },
     {
       file: "dist/index.cjs.js",
       format: "cjs",
@@ -13,16 +17,18 @@ const config = {
     },
     {
       file: "dist/index.esm.js",
-      format: "esm",
+      format: "es",
       sourcemap: true,
     },
   ],
-  plugins: [PeerDepsExternal(), resolve(), commonjs(), copy({
-    targets: [
-      { src: "src/index.d.ts", dest: 'dist/'}
-    ]
-  })],
-}
-
+  plugins: [
+    PeerDepsExternal(),
+    resolve(),
+    commonjs(),
+    copy({
+      targets: [{ src: "src/index.d.ts", dest: "dist/" }],
+    }),
+  ],
+};
 
 export default config;
